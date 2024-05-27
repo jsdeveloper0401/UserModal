63d674ba88aea26ad9811a69c560a8e77283314a
jsdeveloper0401 <wolfmaks85@gmail.com>
2024-05-27 22:15:06 +0500

    dfsf

--- src/components/login/login.jsx
+++ src/components/login/login.jsx
@@ -3,47 +3,65 @@ import { useNavigate } from "react-router-dom";
 import "./login.css";
 
 const Login = () => {
-  
-  const [form,setForm] = useState({})
-  const navigate = useNavigate()
+    const [form, setForm] = useState({ username: "", password: "" });
+    const navigate = useNavigate();
 
-  const handleChange =(e)=>{
-    const {name,value}= e.target
-    setForm({...form,[name]:value})
-  }
+    const handleChange = (e) => {
+        const { name, value } = e.target;
+        setForm({ ...form, [name]: value });
+    };
 
-  const handleSubmit = (e) => {
-    e.preventDefault()
-    const {username,password} = form
-    if(username ==="admin" && password === "123"){
-      navigate("/cars")
-    }else{
-      alert("404 => Something went wrong")
-    }
-    console.log(form);
-  };
+    const handleSubmit = (e) => {
+        e.preventDefault();
+        const { username, password } = form;
+        if (username === "admin" && password === "123") {
+            navigate("/cars");
+        } else {
+            alert("404 => Something went wrong");
+        }
+        setForm({ username: "", password: "" });
+    };
     return (
-      <div className="container">
-        <div className="row">
-          <div className="col-md-6 offset-3 mt-5">
-            <form id="form" onSubmit={handleSubmit}>
-              <div className="card">
-                <div className="card-header">
-                  <h1 className="text-center">Login</h1>
+        <div className="container">
+            <div className="row">
+                <div className="col-md-6 offset-3 mt-5">
+                    <form id="form" onSubmit={handleSubmit}>
+                        <div className="card">
+                            <div className="card-header">
+                                <h1 className="text-center">Login</h1>
+                            </div>
+                            <div className="card-body">
+                                <input
+                                    value={form.username}
+                                    onChange={handleChange}
+                                    type="text"
+                                    className="form-control my-2"
+                                    name="username"
+                                    placeholder="Username"
+                                />
+                                <input
+                                    value={form.password}
+                                    onChange={handleChange}
+                                    type="password"
+                                    className="form-control my-2"
+                                    name="password"
+                                    placeholder="Password"
+                                />
+                            </div>
+                            <div className="card-footer">
+                                <button
+                                    className="btn btn-info"
+                                    type="submit"
+                                    form="form">
+                                    Login
+                                </button>
+                            </div>
+                        </div>
+                    </form>
                 </div>
-                <div className="card-body">
-                  <input onChange={handleChange} type="text" className="form-control my-2" name="username" placeholder="Username"/>
-                  <input onChange={handleChange} type="password" className="form-control my-2" name="password" placeholder="Password"/>
-                </div>
-                <div className="card-footer">
-                  <button className="btn btn-info" type="submit" form="form">Login</button>
-                </div>
-              </div>
-            </form>
-          </div>
+            </div>
         </div>
-      </div>
-    )
+    );
 };
 
 export default Login;
--- src/components/modal.jsx
+++ src/components/modal.jsx
@@ -1,10 +1,82 @@
-import { Modal } from "reactstrap"
-const UserModal = ({open,toggle}) => {
-  return (
-      <Modal isOpen={open} toggle={toggle}>
-        <h1>Add User</h1>
-      </Modal>
-  )
-}
 
-export default UserModal
+
+import { useState } from "react";
+import { Modal, ModalBody, ModalHeader, ModalFooter } from "reactstrap";
+
+const UserModal = ({ open, toggle, cars, setCars }) => {
+    const [form, setForm] = useState({});
+
+    const handleChange = (e) => {
+        const { name, value } = e.target;
+        setForm({ ...form, [name]: value });
+    };
+
+    const handleSubmit = (e) => {
+        e.preventDefault();
+        if (Object.values(form).some((value) => !value.trim())) {
+            alert("Malumot kiritilmagan yoki bo'sh bo'lishi mumkin emas");
+            return;
+        }
+        setCars([...cars, form]);
+        e.target.reset();
+        setForm({});
+        toggle();
+    };
+
+    return (
+        <Modal isOpen={open} toggle={toggle}>
+            <ModalHeader>
+                <h1 className="text-center font-monospace">Add user</h1>
+            </ModalHeader>
+            <ModalBody>
+                <form id="form" onSubmit={handleSubmit}>
+                    <input
+                        onChange={handleChange}
+                        type="text"
+                        placeholder="Name"
+                        name="name"
+                        className="form-control my-2"
+                    />
+                    <input
+                        onChange={handleChange}
+                        type="text"
+                        placeholder="Brand"
+                        name="brand"
+                        className="form-control my-2"
+                    />
+                    <input
+                        onChange={handleChange}
+                        type="text"
+                        placeholder="Color"
+                        name="color"
+                        className="form-control my-2"
+                    />
+                    <input
+                        onChange={handleChange}
+                        type="number"
+                        placeholder="Price"
+                        name="price"
+                        className="form-control my-2"
+                    />
+                    <input
+                        onChange={handleChange}
+                        type="date"
+                        placeholder="Year"
+                        name="year"
+                        className="form-control my-2"
+                    />
+                </form>
+            </ModalBody>
+            <ModalFooter>
+                <button className="btn btn-danger" onClick={toggle}>
+                    Cancel
+                </button>
+                <button className="btn btn-success" type="submit" form="form">
+                    Save
+                </button>
+            </ModalFooter>
+        </Modal>
+    );
+};
+
+export default UserModal;
